53d60cca1c20b5b7b1c689ff2566195ab6e99d95
jayashan <jguruge2021@gmail.com>
2025-05-18 20:00:35 +0530

    Check Connection in connection.php

--- connect.php
+++ connect.php
@@ -6,6 +6,14 @@ $db = "mybook_db";
 
 // Connect to the database
 $connection = mysqli_connect($host, $username, $password, $db);
+
+// Check the connection
+if (!$connection) {
+    die("Connection failed: " . mysqli_connect_error());
+}
+echo "✅ Connected to the database successfully!";
+
 // Close the connection
 mysqli_close($connection);
+//comment for test
 ?>
